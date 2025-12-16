751af40e17d4484ed0a481feec2679d6a2b4b8fb
oliwermiller <oliwer.miller@student.put.poznan.pl>
2025-12-16 22:06:44 +0100

    Poprawiłem maina w aplikacji

--- AplikacjaBankowa.java
+++ AplikacjaBankowa.java
@@ -1,5 +1,5 @@
 public class AplikacjaBankowa{
-    public static void Main(String[] args){
-        
-    }
+    public static void main(String[] args){
+        //System.out.println("wszystko git jeśli to się wyświetla");
+}
 }
