86e839c920f3e8bd85031ff9b9005c8ac77e66bf
oliwermiller <oliwer.miller@student.put.poznan.pl>
2025-12-17 16:44:27 +0100

    funkcja factory

--- AplikacjaBankowa.java
+++ AplikacjaBankowa.java
@@ -1,5 +1,23 @@
-public class AplikacjaBankowa{
-    public static void main(String[] args){
-        //System.out.println("wszystko git jeśli to się wyświetla");
-}
+public class AplikacjaBankowa {
+    public static void main(String[] args) {
+        // Pobranie instancji serwisu
+        BankService bankService = BankService.getInstance();
+
+        // Tworzenie kont za pomocą Factory
+        Konto mojeOsobiste = bankService.stworzKonto("OSOBISTE", 1000.0, 500.0);
+        Konto mojeOszczednosci = bankService.stworzKonto("OSZCZEDNOSCIOWE", 5000.0, 0.05);
+
+        // Testowanie działań
+        System.out.println("--- Test Konta Osobistego ---");
+        mojeOsobiste.wyplac(1200.0); // Powinno wejść w debet
+        
+        System.out.println("\n--- Test Konta Oszczędnościowego ---");
+        mojeOszczednosci.wplac(500.0);
+        
+        // Wywołanie metody specyficznej dla klasy (wymaga rzutowania)
+        if (mojeOszczednosci instanceof KontoOszczednosciowe) {
+            ((KontoOszczednosciowe) mojeOszczednosci).naliczOprocentowanie();
+            System.out.println("Po naliczeniu odsetek: " + mojeOszczednosci.getSaldo());
+        }
+    }
 }
--- BankService.java
+++ BankService.java
@@ -1,10 +1,32 @@
-public class BankService{
-    public static BankService instance;
-    private BankService(){};
-    public static BankService getInstance(){
-        if(instance == null){
+public class BankService {
+    private static BankService instance;
+
+    private BankService() {}
+
+    public static BankService getInstance() {
+        if (instance == null) {
             instance = new BankService();
         }
         return instance;
+    }
+
+    /**
+     * Metoda Factory do tworzenia kont.
+     * @param typ "OSOBISTE" lub "OSZCZEDNOSCIOWE"
+     * @param saldoPoczatkowe kwota na start
+     * @param parametrDodatkowy limit debetu dla osobistego LUB oprocentowanie dla oszczędnościowego
+     */
+    public Konto stworzKonto(String typ, double saldoPoczatkowe, double parametrDodatkowy) {
+        if (typ == null) {
+            return null;
+        }
+        
+        if (typ.equalsIgnoreCase("OSOBISTE")) {
+            return new KontoOsobiste(saldoPoczatkowe, parametrDodatkowy);
+        } else if (typ.equalsIgnoreCase("OSZCZEDNOSCIOWE")) {
+            return new KontoOszczednosciowe(saldoPoczatkowe, parametrDodatkowy);
+        }
+        
+        throw new IllegalArgumentException("Nieznany typ konta: " + typ);
     }
 }
