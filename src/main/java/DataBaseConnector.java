a5de6da2a686e11445523ccefbc61d37e08ee572
oliwermiller <oliwer.miller@student.put.poznan.pl>
2025-12-18 22:22:18 +0100

    zmiana ścieżek

--- src/main/java/DataBaseConnector.java
+++ src/main/java/DataBaseConnector.java
@@ -3,7 +3,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnector {
-    private static final String URL = "jdbc:sqlite:C:/Dokum/PO/bazaKont.db";
+    //private static final String URL = "jdbc:sqlite:C:/Dokum/PO/bazaKont.db";
+    private static final String URL = "jdbc:sqlite:bazaKont.db";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
--- src/main/java/HistoriaDBConnector.java
+++ src/main/java/HistoriaDBConnector.java
@@ -4,6 +4,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class HistoriaDBConnector {
+    //private static final String URL = "jdbc:sqlite:HistoriaTransakcji.db";
     private static final String URL = "jdbc:sqlite:HistoriaTransakcji.db";
 
     public static Connection getConnection() throws SQLException {
